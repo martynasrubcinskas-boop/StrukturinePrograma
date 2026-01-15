7d0a889801d954328433ef381cccfd1cd7470e0f
martynasrubcinskas-boop <martynas.rubcinskas@gmail.com>
2026-01-16 00:31:07 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,42 +1,107 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
 using namespace std;
 
+const int MAX_ITEMS = 20;
+const double TAX = 0.21;
+
+struct menuItemType {
+    string menuItem;
+    double menuPrice;
+};
+
+int getData(menuItemType menu[]);
+void showMenu(menuItemType menu[], int n);
+void printCheck(menuItemType menu[], int n, int qty[]);
+
 int main() {
+    menuItemType menu[MAX_ITEMS];
+    int qty[MAX_ITEMS] = {0};
 
-     string masyvas[6];
+    int count = getData(menu);
 
-     for (int i = 0; i < 6; i++) {
-         cout << "Mokiniu vardai: " << endl;
-         cin >> masyvas[i];
-     }
-
-    int pazymiai;
-    string mokiniai;
-    int masyvas2[10];
-
-    cout << "Kiek Mokiniu: " << endl;
-    cin >> mokiniai;
-    cout << "Kiek Pazymiu: " << endl;
-    cin >> pazymiai;
-
-    pazymiai;
-    for (int j = 0; j < pazymiai; j++) {
-        cout << "Pazymis: "<< endl;
-        cin >> masyvas2[pazymiai-1];
+    if (count == 0) {
+        cout << "Klaida: menu.txt neatrastas\n";
+        return 1;
     }
 
-    int e;
-    cout << "Mokinio pazymis: " << endl;
-    cin >> e;
+    showMenu(menu, count);
 
-        for (int j = 0; j < pazymiai; j++) {
-            if (masyvas2[j] == e) {
-                cout << " Pazymys: " << j+1 << endl;
-            }
-        }
+    cout << "\nIveskite kieki (0 jeigu nereikia):\n";
+    for (int i = 0; i < count; i++) {
+        cout << menu[i].menuItem << ": ";
+        cin >> qty[i];
+    }
 
-        for (int i = 0; i < 6; i++) {
-            if (masyvas[6] == e) {}
-            cout << "Mokinys: " << i+1 << endl;
+    printCheck(menu, count, qty);
+    return 0;
+}
+
+int getData(menuItemType menu[]) {
+    ifstream file("menu.txt");
+    if (!file) return 0;
+
+    int i = 0;
+    string line;
+
+    while (getline(file, line) && i < MAX_ITEMS) {
+        int pos = line.find_last_of(' ');
+        if (pos == string::npos) continue;
+
+        menu[i].menuItem = line.substr(0, pos);
+        menu[i].menuPrice = stod(line.substr(pos + 1));
+        i++;
+    }
+
+    file.close();
+    return i;
+}
+
+void showMenu(menuItemType menu[], int n) {
+    cout << "\nSveiki atvyke i Prancuziska Restorana\n\n";
+    cout << fixed << setprecision(2);
+
+    for (int i = 0; i < n; i++) {
+        cout << setw(2) << i + 1 << ". "
+             << setw(20) << left << menu[i].menuItem
+             << menu[i].menuPrice << " EUR\n";
+    }
+}
+
+void printCheck(menuItemType menu[], int n, int qty[]) {
+    ofstream receipt("receipt.txt");
+    double sum = 0.0;
+
+    cout << "\n--- Saskaita ---\n";
+    receipt << "--- Saskaita ---\n";
+
+    for (int i = 0; i < n; i++) {
+        if (qty[i] > 0) {
+            double price = qty[i] * menu[i].menuPrice;
+            sum += price;
+
+            cout << qty[i] << " "
+                 << setw(20) << left << menu[i].menuItem
+                 << price << " EUR\n";
+
+            receipt << qty[i] << " "
+                    << setw(20) << left << menu[i].menuItem
+                    << price << " EUR\n";
         }
     }
+
+    double tax = sum * TAX;
+    double total = sum + tax;
+
+    cout << "\nPVM (21%): " << tax << " EUR\n";
+    cout << "Suma: " << total << " EUR\n";
+
+    receipt << "\nPVM (21%): " << tax << " EUR\n";
+    receipt << "Suma: " << total << " EUR\n";
+
+    receipt.close();
+}
+
